bfbc0520a665bafb10f280d9ad13854abaef29ce
Beverly Appleton <bfcain19@gmail.com>
2020-03-25 16:55:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,3 +6,5 @@ end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I a m learning to program in #{language}.\n"
 end
+
+introduction_with_language("Dan", "Ember.js")
